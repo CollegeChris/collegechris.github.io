d396015954e9858e7189c29a7fb95c6a8f3db0af
CollegeChris <christopher.towns@dcmail.ca>
2025-11-12 15:46:10 -0500

    Setting up assignment 4 part 2 and fixed missing tag in part 1

---
+++ assignment4/part2/js/script.js
@@ -0,0 +1,28 @@
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+const images = [
+	{
+		src: "pic1.jpg",
+		alt: "Closeup of a human eye"
+	},
+	{
+		src: "pic2.jpg",
+		alt: "Rock that looks like a wave"
+	},
+	{
+		src: "pic3.jpg",
+		alt: "Purple and white pansies"
+	},
+	{
+		src: "pic4.jpg",
+		alt: "Section of wall from a pharaoh's tomb"
+	},
+	{
+		src: "pic5.jpg",
+		alt: "Large moth on a leaf"
+	},	
+];
